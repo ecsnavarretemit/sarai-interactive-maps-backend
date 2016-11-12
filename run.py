0cb25b369ac2769f1e808a5811e3879b0ca618b3
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-11-12 13:45:14 +0800

    Add config for Flask host settings

--- run.py
+++ run.py
@@ -10,6 +10,6 @@ from app import app
 
 if __name__ == "__main__":
   # run the application
-  app.run(debug=app.config['DEBUG'], port=app.config['PORT'])
+  app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
 
 
