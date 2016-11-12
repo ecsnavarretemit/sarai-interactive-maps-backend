1df09675f65bb96758631f5630ea53a608ba08ad
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-11-12 10:23:23 +0800

    Add api endpoint for retrieving provinces within specific regions

--- app/views/regions.py
+++ app/views/regions.py
@@ -7,7 +7,7 @@
 from flask import Blueprint, jsonify, abort
 from flask_cors import cross_origin
 from app.models import Region
-from app.schema import RegionSchema
+from app.schema import RegionSchema, ProvinceSchema
 
 mod = Blueprint('regions', __name__, url_prefix='/regions')
 
@@ -44,6 +44,26 @@ def by_id(region_id):
 
   return jsonify(response)
 
+@mod.route('/<region_id>/provinces', methods=['GET'])
+def get_provinces(region_id):
+  region = Region.query.get(region_id)
+
+  response = {
+    'success': True
+  }
+
+  # invoke the page not found handler when region is not found
+  if region is None:
+    abort(404, 'Region not found')
+
+  provinces = region.provinces.all()
+
+  province_schema = ProvinceSchema(many=True)
+  result = province_schema.dump(provinces)
+  response['result'] = result.data
+
+  return jsonify(response)
+
 @mod.route('/slug/<slug>', methods=['GET'])
 def by_slug(slug):
   region = Region.query.filter_by(slug=slug).first()
@@ -58,6 +78,26 @@ def by_slug(slug):
 
   region_schema = RegionSchema()
   result = region_schema.dump(region)
+  response['result'] = result.data
+
+  return jsonify(response)
+
+@mod.route('/slug/<slug>/provinces', methods=['GET'])
+def get_provinces_by_region_slug(slug):
+  region = Region.query.filter_by(slug=slug).first()
+
+  response = {
+    'success': True
+  }
+
+  # invoke the page not found handler when region is not found
+  if region is None:
+    abort(404, 'Region not found')
+
+  provinces = region.provinces.all()
+
+  province_schema = ProvinceSchema(many=True)
+  result = province_schema.dump(provinces)
   response['result'] = result.data
 
   return jsonify(response)
