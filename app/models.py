9d55d97c1394a7f86f3b565af8170058377f1ad3
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-11-12 11:21:37 +0800

    Bump version to 1.0.0-alpha1

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 #
 # Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
 # Licensed under MIT
-# Version 0.0.0
+# Version 1.0.0-alpha1
 
 from app import db
 from slugify import slugify
