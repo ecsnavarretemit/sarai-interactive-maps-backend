d3b020a300b79dce486b82bded9d4f00b143fe57
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-03-25 19:50:05 +0800

    Implement NDVI DOY export data

--- app/views/ndvi.py
+++ app/views/ndvi.py
@@ -7,6 +7,7 @@
 import ee
 import csv
 import StringIO
+from datetime import datetime
 from flask import Blueprint, jsonify, request, abort, make_response
 from flask_cors import cross_origin
 from app.gzipped import gzipped
@@ -55,7 +56,7 @@ def ndvi_cache_key(*args, **kwargs):
   return (path + args).encode('utf-8')
 
 def query_time_series_data(lat, lng, start_date, end_date):
-  cache_key = 'time_series_%s_%s_%s_%s' % (lat, lng, start_date, end_date)
+  cache_key = 'ndvi_time_series_%s_%s_%s_%s' % (lat, lng, start_date, end_date)
 
   final_result = cache.get(cache_key)
 
@@ -79,10 +80,35 @@ def query_time_series_data(lat, lng, start_date, end_date):
 
     final_result = map(time_series_mapper, result)
 
-    # cache it for 1 year
+    # cache it for 12 hours
     cache.set(cache_key, final_result, timeout=43200)
 
   return final_result
+
+def query_doy_data(lat, lng, start_date, end_date):
+  cache_key = 'ndvi_doy_%s_%s_%s_%s' % (lat, lng, start_date, end_date)
+
+  processed = cache.get(cache_key)
+
+  # perform query processing when its not on the cache
+  if processed is None:
+    query_result = query_time_series_data(lat, lng, start_date, end_date)
+
+    processed = {}
+    for value in query_result:
+      parsed_date = datetime.strptime(value['time'], '%Y-%m-%d')
+      doy = int(parsed_date.strftime('%j'))
+      year = parsed_date.strftime('%Y')
+
+      if processed.get(year) is None:
+        processed[year] = {}
+
+      processed[year][doy] = value['ndvi']
+
+    # cache it for 12 hours
+    cache.set(cache_key, processed, timeout=43200)
+
+  return processed
 
 # cache the result of this endpoint for 12 hours
 @mod.route('/<start_date>/<end_date>', methods=['GET'])
@@ -170,7 +196,67 @@ def time_series(lat, lng, start_date, end_date):
         value['ndvi']
       ])
 
-    filename = 'ndvi-%s-%s-%s-%s' % (lat, lng, start_date, end_date)
+    filename = 'ndvi-time-series-%s-%s-%s-%s' % (lat, lng, start_date, end_date)
+
+    response = make_response(si.getvalue())
+    response.headers['Content-Disposition'] = 'attachment; filename=%s.csv' % filename
+    response.headers['Content-type'] = 'text/csv'
+
+  return response
+
+@mod.route('/day-of-the-year/<lat>/<lng>/<start_date>/<end_date>', methods=['GET'])
+@cross_origin()
+@gzipped
+def day_of_the_year(lat, lng, start_date, end_date):
+  query_result = query_doy_data(lat, lng, start_date, end_date)
+  output_format = 'json'
+  available_formats = ['json', 'csv']
+  requested_format = request.args.get('fmt')
+
+  if requested_format is not None:
+    # abort the request and throw HTTP 400 since the format
+    # is not on the list of available formats
+    if not requested_format in available_formats:
+      abort(400, 'Unsupported format')
+
+    # override the default output format
+    output_format = requested_format
+
+  response = None
+
+  if output_format == 'json':
+    json_result = {
+      'success': True,
+      'result': query_result
+    }
+
+    response = jsonify(**json_result)
+  else:
+    si = StringIO.StringIO()
+    cw = csv.writer(si)
+
+    csv_header_row = query_result.keys()
+
+    # sort the items
+    csv_header_row.sort()
+
+    # save the sorted keys
+    years = csv_header_row[:]
+
+    # add the DOY to the first element of the row
+    csv_header_row.insert(0, 'Day of the Year')
+
+    # loop through all the doy and years and assemble the csv rows
+    cw.writerow(csv_header_row)
+    for doy in range(1, 365, 16):
+      row = [doy]
+
+      for year in years:
+        row.append(query_result[year].get(doy, ''))
+
+      cw.writerow(row)
+
+    filename = 'ndvi-doy-%s-%s-%s-%s' % (lat, lng, start_date, end_date)
 
     response = make_response(si.getvalue())
     response.headers['Content-Disposition'] = 'attachment; filename=%s.csv' % filename
